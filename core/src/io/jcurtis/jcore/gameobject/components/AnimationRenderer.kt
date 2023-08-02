92c1ad32e8b954c7758082ad19026aad59a61c35
jcurtis06 <06jcurtis@gmail.com>
2023-08-02 10:02:41 -0400

    added animations

---
+++ core/src/io/jcurtis/jcore/gameobject/components/AnimationRenderer.kt
@@ -0,0 +1,61 @@
+package io.jcurtis.jcore.gameobject.components
+
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import io.jcurtis.jcore.graphics.Renderable
+import io.jcurtis.platformer.graphics.AnimatedSpriteSheet
+
+// Animated sprite sheet = stores frames
+// Animation renderer = animates frames and picks which sprite sheet to render
+
+class AnimationRenderer: Component(), Renderable {
+    private var spriteSheetMap = mutableMapOf<String, AnimatedSpriteSheet>()
+    private var animation: Animation<TextureRegion>? = null
+
+    var flipH = false
+
+    private var currentAnimation: AnimatedSpriteSheet? = null
+        set(value) {
+            animation = Animation(value!!.duration, *value.splitAnimation())
+            field = value
+        }
+
+
+    private var stateTime = 0f
+
+    fun play(animation: String) {
+        currentAnimation = spriteSheetMap[animation]
+    }
+
+    fun addAnimation(name: String, sheet: AnimatedSpriteSheet) {
+        spriteSheetMap[name] = sheet
+    }
+
+    private fun getCurrentFrame(): TextureRegion {
+        val currentFrame = animation!!.getKeyFrame(stateTime, true)
+
+        if (flipH && !currentFrame.isFlipX) {
+            currentFrame.flip(true, false)
+        } else if (!flipH && currentFrame.isFlipX) {
+            currentFrame.flip(true, false)
+        }
+
+        return currentFrame
+    }
+
+    override fun update(delta: Float) {
+        stateTime += delta
+    }
+
+    override fun render(batch: SpriteBatch) {
+        batch.draw(getCurrentFrame(), transform.position.x, transform.position.y)
+    }
+
+    override fun setView(camera: OrthographicCamera) {
+    }
+
+    override fun init() {
+    }
+}
--- core/src/io/jcurtis/jcore/test/Main.kt
+++ core/src/io/jcurtis/jcore/test/Main.kt
@@ -7,14 +7,17 @@ import io.jcurtis.jcore.core.Core
 import io.jcurtis.jcore.core.JCoreGame
 import io.jcurtis.jcore.gameobject.GameObject
 import io.jcurtis.jcore.gameobject.components.*
+import io.jcurtis.platformer.graphics.AnimatedSpriteSheet
 
 class Main: JCoreGame() {
     private val player: GameObject = GameObject()
     private val map: GameObject = GameObject()
 
     override fun init() {
+        showCollisionBoxes = false
         Core.assets.setLoader(TiledMap::class.java, TmxMapLoader())
         Core.assets.load("slime.png", Texture::class.java)
+        Core.assets.load("player.png", Texture::class.java)
         Core.assets.load("badlogic.jpg", Texture::class.java)
         Core.assets.load("test.tmx", TiledMap::class.java)
         Core.assets.finishLoading()
@@ -25,12 +28,28 @@ class Main: JCoreGame() {
         map.attach<TilemapCollider>()
 
         player.attach<PlayerController>()
-        player.attach<Image>().setTexture(Core.assets.get("slime.png", Texture::class.java))
+        player.attach<AnimationRenderer>().apply {
+            addAnimation(
+                "idle",
+                AnimatedSpriteSheet(
+                    Core.assets.get("player.png", Texture::class.java),
+                    7,
+                    1,
+                    0.1f,
+                    0,
+                    0,
+                    3,
+                    1
+                )
+            )
+        }
         player.attach<BoxCollider>().apply {
             width = 14f
             height = 12f
         }
         player.attach<RigidBody>()
 
+        player.getComponent<AnimationRenderer>()?.play("idle")
+        player.getComponent<AnimationRenderer>()?.flipH = true
     }
 }
